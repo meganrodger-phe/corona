e30cecdf6b2f3d602f5ed78e2e4b6739ee03c796
Megan Rodger <megan.rodger@phe.gov.uk>
2020-03-06 12:04:10 +0000

    first pages making a little sense

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'COVID-19 Contact tracing tool',
 
   // Default port that prototype runs on
   port: '3000',
