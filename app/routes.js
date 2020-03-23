97708e02faef4a5188cd6c18b3b08ed56fb2d529
Megan Rodger <megan.rodger@phe.gov.uk>
2020-03-23 07:40:25 +0000

    getting routes in

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,26 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+// Branching
+router.post('/tested-positive/symptoms', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let statements = req.session.data['statements']
+
+  if (statements === 'results') {
+    res.redirect('/awaiting-results/symptoms-waiting')
+
+  } else if (statements === 'contact') {
+    res.redirect('/contact-of/self-isolate-q')
+
+  } else if (statements === 'traveller')  {
+    res.redirect('/returning-abroad/self-isolate')
+  }
+  else {
+    res.redirect('/tested-positive/symptoms')
+  }
+
+})
