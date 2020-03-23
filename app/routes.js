57d9cb07972e3342d998c88f7b7be8fea9d1954a
Megan Rodger <megan.rodger@phe.gov.uk>
2020-03-23 08:11:49 +0000

    contact route in

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,7 @@ router.post('/tested-positive/symptoms', function (req, res) {
   // The name between the quotes is the same as the 'name' attribute on the input elements
   // However in JavaScript we can't use hyphens in variable names
 
+
   let statements = req.session.data['statements']
 
   if (statements === 'results') {
@@ -25,5 +26,32 @@ router.post('/tested-positive/symptoms', function (req, res) {
   else {
     res.redirect('/tested-positive/symptoms')
   }
+
+})
+
+
+router.post('/contact-of/self-isolate', function (req, res) {
+
+let self_isolate = req.session.data['self_isolate']
+
+if (self_isolate === 'no-not') {
+  res.redirect('/contact-of/contact-preferences')
+}
+else {
+  res.redirect('/contact-of/self-isolate')
+}
+
+})
+
+router.post('/contact-of/symptoms-contact', function (req, res) {
+
+let symptoms_q = req.session.data['symptoms_q']
+
+if (symptoms_q === 'no_symptoms') {
+  res.redirect('/contact-of/thank-you-helping')
+}
+else {
+  res.redirect('/contact-of/symptoms-contact')
+}
 
 })
